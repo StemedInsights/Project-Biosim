5e404ca38598b23623ced2ad7a44d2e964aac8a0
Pranav Moorthy <pranavsmoorthy@gmail.com>
2025-04-14 06:05:53 -0400

    moved backend to server folder and added client + react template

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -14,7 +14,7 @@ const createCourse = asyncHandler(async (req, res) => {
 
         const course = await Course.create(data);
         res.status(200).json(course);
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         res.status(500).json({
             "code": 'course-create-failed',
@@ -29,9 +29,9 @@ const getCourses = asyncHandler(async (req, res) => {
 })
 
 const getCourseById = asyncHandler(async (request, response) => {
-    try{
+    try {
         const course = await Course.findById(request.params.id);
-        if(Utils.isEmptyOrNil(course)){
+        if (Utils.isEmptyOrNil(course)) {
             response.status(constants.NOT_FOUND).json({
                 "code": 'course-not-found',
                 "messages": ["Could not find course with given id: " + request.params.id]
@@ -40,7 +40,7 @@ const getCourseById = asyncHandler(async (request, response) => {
         }
 
         response.status(200).json(course);
-    }catch(err){
+    } catch (err) {
         console.log(err);
         response.status(500).json({
             "code": 'course-get-failed',
@@ -50,11 +50,11 @@ const getCourseById = asyncHandler(async (request, response) => {
 });
 
 const getCourseByService = asyncHandler(async (request, response) => {
-    try{
+    try {
         const query = { "service": request.params.service }
         const courses = await Course.find(query);
 
-        if(Utils.isEmptyOrNil(courses)){
+        if (Utils.isEmptyOrNil(courses)) {
             response.status(constants.NOT_FOUND).json({
                 "code": 'course-not-found',
                 "messages": ["Could not find course with given service: " + request.params.service]
@@ -63,7 +63,7 @@ const getCourseByService = asyncHandler(async (request, response) => {
         }
 
         response.status(200).json(courses);
-    }catch(err){
+    } catch (err) {
         console.log(err);
         response.status(500).json({
             "code": 'course-get-failed',
@@ -73,17 +73,17 @@ const getCourseByService = asyncHandler(async (request, response) => {
 });
 
 const updateCourseById = asyncHandler(async (request, response) => {
-    try{
+    try {
         const course = await Course.findById(request.params.id);
-        
-        if(Utils.isEmptyOrNil(course)){
+
+        if (Utils.isEmptyOrNil(course)) {
             response.status(404).json({
                 "code": 'course-not-found',
                 "messages": ["Could not find course with given id: " + request.params.id]
             });
             return;
         }
-        
+
         const updatedCourse = await Course.findByIdAndUpdate(
             request.params.id,
             request.body,
@@ -91,7 +91,7 @@ const updateCourseById = asyncHandler(async (request, response) => {
         );
 
         response.status(200).json(updatedCourse);
-    }catch(err){
+    } catch (err) {
         response.status(500).json({
             "code": 'user-update-failed',
             "messages": ['Internal Server Error: Unable to update course']
@@ -100,10 +100,10 @@ const updateCourseById = asyncHandler(async (request, response) => {
 })
 
 const deleteCourseById = asyncHandler(async (request, response) => {
-    try{
+    try {
         const course = await Course.findById(request.params.id);
-        
-        if(Utils.isEmptyOrNil(course)){
+
+        if (Utils.isEmptyOrNil(course)) {
             response.status(404).json({
                 "code": 'course-delete-failed',
                 "messages": ["Unable to find course with given id"]
@@ -113,7 +113,7 @@ const deleteCourseById = asyncHandler(async (request, response) => {
 
         await Course.deleteOne(course);
         response.status(200).json(course);
-    }catch(err){
+    } catch (err) {
         response.status(404).json({
             "code": 'course-delete-failed',
             "messages": ["Unable to find course with given id"]
--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const connectDb = require("./config/dbConnection");
-const detenv = require("dotenv").config();
+const dotenv = require("dotenv").config();
+const path = require('path');
+const cors = require("cors");
+
+const corsOptions = {
+    origin: ["http://localhost:5173", "http://localhost:5001"]
+}
 
 const port = process.env.PORT || 5000;
 const app = express();
 connectDb();
+
+app.use(cors(corsOptions));
+app.use(express.static("public"));
 
 app.use(express.json());
 app.use("/api/biosim/course", require("./routes/courseRoutes"));
